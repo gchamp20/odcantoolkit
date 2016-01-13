5197248cb8a61b6e536ae9f0f0f1ceddbba4a26d
Guillaume <champagne.guillaume.c@gmail.com>
2016-01-12 22:30:56 -0500

    Publishing release branch for release 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='odcantoolkit',
-    version='0.1',
+    version='0.1.1',
     description='Python tool to fetch data from Canada\'s open data portal',
     long_description=long_description,
 
